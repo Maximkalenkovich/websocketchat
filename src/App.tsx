edc5cc09fe1946ed8f0adaf72a355387e4a2633b
Maximkalenkovich <peterson.maxim@mail.ru>
2024-07-08 11:55:38 +0300

    add designm messenger

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import s from './app.module.css'
+
+const arrMessage = [
+    {message: "test", id: '4564',user:{id:'234234we', name:"Dima"}},
+    {message: "hello pedik", id: 'wsfwe32',user:{id:'wefwe43', name:"mAX"}}
+
+]
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [messages,setMessages] = useState(arrMessage)
+
+
+    return (
+        <div className="App">
+            {
+                messages.map((message) => {
+                    return (
+                        <div className={s.message} key={message.id}>
+                            <div>
+                                <div className={s.message__username}>{message.user.name}</div>
+                                {message.message}
+                            </div>
+                        </div>
+                    )
+                })
+            }
+            <div className={s.inputGroup}>
+                <textarea  className={s.input}/>
+                <button className={s.buttons}>отправить</button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
